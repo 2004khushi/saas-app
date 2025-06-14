dbbada6eee5487f8609c86f77ae442135bdc68cc
2004khushi <goyalkhushi1004@gmail.com>
2025-06-14 21:44:53 +0530

    implement session history

--- app/page.tsx
+++ app/page.tsx
@@ -1,55 +1,39 @@
-import React from 'react'
 import CompanionCard from "@/components/CompanionCard";
-import CTA from "@/components/CTA";
 import CompanionsList from "@/components/CompanionsList";
+import CTA from "@/components/CTA";
 import {recentSessions} from "@/constants";
+import {getAllCompanions, getRecentSessions} from "@/lib/actions/companion.actions";
+import {getSubjectColor} from "@/lib/utils";
 
-const Page = () => {
-  return (
-    <main>
-      <h1 className="text-2xl underline ">
-        Popular Companions.
-      </h1>
+const Page = async () => {
+    const companions = await getAllCompanions({ limit: 3 });
+    const recentSessionsCompanions = await getRecentSessions(10);
 
-      <section className="home-section">
-          <CompanionCard
-              id="123"
-              name="Neura The Brainy Explorer"
-              topic="Neural Network of the Brain"
-              subject="Science"
-              duration={45}
-              color="#ffda6e"
-          />
-          <CompanionCard
-              id="234"
-              name="Countsy the number Wizard"
-              topic="Derivatives and Integrals"
-              subject="Maths"
-              duration={50}
-              color="#e5d0ff"
-          />
-          <CompanionCard
-              id="345"
-              name="Verba the Vocab Builder"
-              topic="language"
-              subject="English Literature"
-              duration={35 }
-              color="#BDE7FF"
-          />
-      </section>
+    return (
+        <main>
+            <h1>Popular Companions</h1>
 
-      <section className="home-section">
+            <section className="home-section">
+                {companions.map((companion) => (
+                    <CompanionCard
+                        key={companion.id}
+                        {...companion}
+                        color={getSubjectColor(companion.subject)}
+                    />
+                ))}
 
-          <CompanionsList
-              title="Recently Completed Sessions"
-              companions={recentSessions}
-              classNames= "w-2/3 max-lg:w-full"
-          />
-          <CTA />
-      </section>
+            </section>
 
-    </main>
-  )
+            <section className="home-section">
+                <CompanionsList
+                    title="Recently completed sessions"
+                    companions={recentSessionsCompanions}
+                    classNames="w-2/3 max-lg:w-full"
+                />
+                <CTA />
+            </section>
+        </main>
+    )
 }
 
 export default Page
--- components/CompanionComponent.tsx
+++ components/CompanionComponent.tsx
@@ -43,7 +43,7 @@ const CompanionComponent = ({ companionId, subject, topic, name, userName, userI
 
         const onCallEnd = () => {
             setCallStatus(CallStatus.FINISHED);
-
+            addToSessionHistory(companionId)
         }
 
         const onMessage = (message: Message) => {
